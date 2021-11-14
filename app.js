cbf0868f01208e65539e528d0769e866b4c11f65
Andrei Sachuk <developer.brest@gmail.com>
2021-11-14 11:45:12 +0300

    added auth back and front

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const config = require('config')
-const mongoose = require('mongoose')
+const express = require( 'express' )
+const config = require( 'config' )
+const mongoose = require( 'mongoose' )
 
 const app = express()
 
-app.use('/api/auth', require('./routes/auth.routes'))
+app.use( express.json( { extended: true } ) )
 
-const PORT = config.get('port') || 5000
+app.use( '/api/auth', require( './routes/auth.routes' ) )
+
+const PORT = config.get( 'port' ) || 5000
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUrl'),{
+        await mongoose.connect( config.get( 'mongoUrl' ), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        })
-        app.listen(PORT, ()=> console.log(`App is started on port ${PORT}`))
+        } )
+        app.listen( PORT, () => console.log( `App is started on port ${PORT}` ) )
     } catch (e) {
-        console.log(`Server error `, e.message)
-        process.exit(1)
+        console.log( `Server error `, e.message )
+        process.exit( 1 )
     }
 }
 
